80754f764483c5dc8c13f746e322317ae2b77f3c
AmaraSesay519 <amarasesay288@gmail.com>
2026-07-02 15:40:23 -0700

    I update the App

--- FIRST_APP/app/(tabs)/index.tsx
+++ FIRST_APP/app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to React Native☠</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.container_body}>
+          <Text style={styles.text}>Welcome to React Native☠</Text>
 
-      <Text style={styles.text}>I will be a app developer</Text>
-    </View>
+          <Text style={styles.text}>I will be a app developer</Text>
+          <Text style={styles.text}>I will be a app developer</Text>
+          <Text style={styles.text}>I will be a app developer</Text>
+          <Text style={styles.text}>I will be a app developer</Text>
+          <Text style={styles.text}>I will be a app developer</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -14,8 +22,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: "#5511c4",
+    backgroundColor: "#5e20c2",
+    borderWidth: 5,
+    borderColor: "#000",
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
   },
+  container_body: {
+    justifyContent: "center",
+    backgroundColor: "#f5f5f5",
+    borderWidth: 5,
+    borderColor: "#000",
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+  },
+
   text: {
     fontSize: 31,
     fontWeight: "bold",
@@ -24,5 +47,10 @@ const styles = StyleSheet.create({
     padding: 20,
     textAlign: "center",
     margin: 15,
+  },
+  sub_cont: {
+    color: "green",
+    fontSize: 16,
+    borderRadius: 10,
   },
 });
